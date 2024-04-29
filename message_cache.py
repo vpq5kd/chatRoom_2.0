0bee5a8a64a77085fd01447f254a611c814a59f3
vpq5kd <vpq5kd@virginia.edu>
2024-04-28 22:18:43 -0400

    wrote the get_messages_by_time method and its associated helper methods

--- message_cache.py
+++ message_cache.py
@@ -1,10 +1,13 @@
 #imports
 import sqlite3
+import datetime
+import pytz
 
 #classes
 
 """class that defines a client message cache, stores the local client messages."""
 class client_message_cache:
+    """initializes the table"""
     def __init__(self,name):
         self.name = name
         #estabish connection
@@ -22,6 +25,8 @@ class client_message_cache:
         #commit and close
         conn.commit()
         conn.close()
+
+    """adds a new message to the database"""
     def add_message(self, sender_name, sender_id, message, time):
         conn = sqlite3.connect('client_message_cache.db')
         cursor = conn.cursor()
@@ -32,8 +37,48 @@ class client_message_cache:
 
         conn.commit()
         conn.close()
+
+    """gets the messages in the database by time"""
     def get_messages_by_time(self):
         conn = sqlite3.connect('client_message_cache.db')
+        cursor = conn.cursor()
+
+        #get messages (by most recent time)
+        cursor.execute(f'''SELECT sender_name, sender_id, message, time FROM {self.name}_client_cache ORDER BY time DESC''')
+        messages = cursor.fetchall()
+
+        formatted_messages = self._format_messages(messages)
+        for formatted_message in formatted_messages:
+            print(formatted_message)
+
+    """formats the database messages for printing to the terminal"""
+    def _format_messages(self, messages):
+        message_string_array = []
+        for message in messages:
+            message_string = ""
+            sender_name = message[0]
+            sender_id = message[1]
+            chat_message = message[2]
+            time = self._translate_epoch_time(message[3])
+
+            message_string+=f"{sender_name} ({sender_id}) sent: {chat_message} at {time}\n"
+            message_delimiter = "-"*50+"\n"
+
+            message_string_array.append(message_string)
+            message_string_array.append(message_delimiter)
+        return message_string_array
+
+    """translates the epoch time to est"""
+    def _translate_epoch_time(self,epoch_time):
+        utc_time = datetime.datetime.utcfromtimestamp(epoch_time)
+        utc_time = utc_time.replace(tzinfo=pytz.utc)
+        est_time = utc_time.astimezone(pytz.timezone('US/Eastern'))
+
+        return est_time
+
+
+
+
 
 
 
