6a86fab2b398b28d0add3ca52801b4af4e36922a
vpq5kd <vpq5kd@virginia.edu>
2024-05-12 10:07:49 -0400

    wrote the split message logic to split each message into it's parts

--- src/main/java/MessageHandler.java
+++ src/main/java/MessageHandler.java
@@ -1,5 +1,16 @@
 public class MessageHandler {
+    private int serverID;
+    private String messageType;
+    private String messageContent;
     public void handleMessage(String message){
+        splitMessage(message);
 
+    }
+
+    private void splitMessage(String message) {
+        String [] parts = message.split("\\*\\*\\*");
+        serverID = Integer.parseInt(parts[1]);
+        messageType = parts[2];
+        messageContent = parts[3];
     }
 }
